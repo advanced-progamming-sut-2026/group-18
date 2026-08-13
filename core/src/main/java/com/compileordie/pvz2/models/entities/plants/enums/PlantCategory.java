7de0efd95dd358f011f0f8b69d7d7d4d0e93ec14
Arsam_Koushki <1385arsam@gmail.com>
2026-08-13 10:48:21 +0330

    resolved merge conflicts in PlantCategory.java

--- core/src/main/java/com/compileordie/pvz2/models/entities/plants/enums/PlantCategory.java
+++ core/src/main/java/com/compileordie/pvz2/models/entities/plants/enums/PlantCategory.java
@@ -1,14 +1,30 @@
 package com.compileordie.pvz2.models.entities.plants.enums;
 
+import com.compileordie.pvz2.utils.Toolbox;
+
 public enum PlantCategory {
-    SUN_PRODUCERS,
-    SHOOTERS,
-    LOBBERS,
-    EXPLOSIVES,
-    MELEE_ATTACKERS,
-    WALL_NUTS,
-    MODIFIERS,
+    SUN_PRODUCER,
+    SHOOTER,
+    LOBBER,
+    EXPLOSIVE,
+    MELEE,        // Changed from MELEE_ATTACKERS
+    WALL_NUT,     // Changed from WALL_NUTS
+    MODIFIER,     // Changed from MODIFIERS
     STRIKE_THROUGH,
     HOMING,
-    MINTS
+    MINT;          // Changed from MINTS
+
+    public static PlantCategory getByName(String name) {
+        for (PlantCategory plantCategory : PlantCategory.values()) {
+            if (plantCategory.toString().equalsIgnoreCase(name)) {
+                return plantCategory;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return Toolbox.enumToString(this, true);
+    }
 }
